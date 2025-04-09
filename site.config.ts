b264df3bda0bd718e7f7ee30a8e632bf9ae314c3
Samysamy57 <152170198+Samysamy57@users.noreply.github.com>
2025-04-09 23:56:36 +0200

    new

--- site.config.ts
+++
@@ -1,8 +0,0 @@
-export const siteConfig = {
-    name: "Babouri Samy  - Portfolio",
-    description: "Wemcome to my Portfolio.",
-    logo: {
-        url: "/logo.png",
-        href: "/logo.png"
-    }
-}
